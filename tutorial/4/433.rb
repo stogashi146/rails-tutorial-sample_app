2e5b3512c1e4baa6e29ba455f38a602cc2b16a75
Ubuntu <ubuntu@ip-172-31-18-4.ec2.internal>
2021-06-24 10:40:18 +0000

    Tutorial Finish 4.3.3

---
+++ tutorial/4/433.rb
@@ -0,0 +1,11 @@
+# 2
+# person1 = {first:"Mito",last:"Tsukino"}
+# person2 = {first:"Kaede",last:"Higuchi"}
+# person3 = {first:"Rin",last:"Sizuka"}
+
+# params = {father:"#{person1}",mother:"#{person2}",chile:"#{person3}"}
+
+# params[:father][:first]
+
+# 3
+user = {name:"Mito",email:"kiritu@gmail.com",password_digest:("a".."z").shuffle[0..16]}
